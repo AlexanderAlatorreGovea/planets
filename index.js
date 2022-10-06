43795fa97fb6246c1231a55a24d4bbc89b0860c5
Alexander <Alexander_alatorre1993@hotmail.com>
2022-10-05 20:52:46 -0500

    added index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const results = [];
+fs.createReadStream("kepler_data.csv")
+  .on("data", (data) => {
+    results.push(data);
+    console.log(data);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", (result) => {
+    console.log(result);
+  });
