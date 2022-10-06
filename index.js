a74f046949c57899af6290447a12fc4f32388fa1
Alexander <Alexander_alatorre1993@hotmail.com>
2022-10-05 21:11:17 -0500

    finished csv validation

--- index.js
+++ index.js
@@ -2,14 +2,34 @@ const { parse } = require("csv-parse");
 const fs = require("fs");
 
 const results = [];
+
+const isHabitable = (planet) =>
+  planet["koi_disposition"] === "CONFIRMED" &&
+  planet["koi_insol"] > 0.36 &&
+  planet["koi_insol"] < 1.11 &&
+  planet["koi_prad"] < 1.6;
+
+// read
 fs.createReadStream("kepler_data.csv")
+  // writable in pipe
+  // pipe the parsed data
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
   .on("data", (data) => {
-    results.push(data);
-    console.log(data);
+    if (isHabitable(data)) {
+      results.push(data);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
-  .on("end", (result) => {
-    console.log(result);
+  .on("end", () => {
+    console.log({
+      results: results.map((planet) => planet.kepler_name),
+      length: results.length,
+    });
   });
